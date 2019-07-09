f923b19da933f7f12cdf63404566f5ba982b97eb
pako herrera <pakman_198@hotmail.com>
2019-07-09 13:03:03 -0700

    Created the recipe model (interface) and displayed recipe items on the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Lemon Pepper Chicken', 
+      'Chicken breast baked with lemon and pepper', 
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/35/1504195270-lemon-pepper-chicken.jpg'
+    ),
+    new Recipe(
+      'Lemon Pepper Chicken', 
+      'Chicken breast baked with lemon and pepper', 
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/35/1504195270-lemon-pepper-chicken.jpg'
+    ),
+  ];
 
   constructor() { }
 
