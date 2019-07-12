9bb440711f44627224ac2aefa68528f64b9ce2ee
pako herrera <pakman_198@hotmail.com>
2019-07-11 18:01:35 -0700

    Added a new ingredients property to the recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     name: string;
     description: string;
     imagePath: string;
+    ingredients: Ingredient[];
     
-    constructor(name: string, desc: string, img: string) {
+    constructor(name: string, desc: string, img: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = img;
+        this.ingredients = ingredients;
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,30 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
-  
+  ingredients: Ingredient[];
+
   private recipes: Recipe[] = [
     new Recipe(
       'Lemon Pepper Chicken', 
       'Chicken breast baked with lemon and pepper', 
-      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/35/1504195270-lemon-pepper-chicken.jpg'
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/35/1504195270-lemon-pepper-chicken.jpg',
+      [
+        new Ingredient('Lemons', 5),
+        new Ingredient('Chicken', 1)
+      ]
     ),
     new Recipe(
       'Lemon Pepper Chicken 2', 
       'Chicken breast baked with lemon and pepper', 
-      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/35/1504195270-lemon-pepper-chicken.jpg'
+      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/35/1504195270-lemon-pepper-chicken.jpg',
+      [
+        new Ingredient('Parcil', 1),
+        new Ingredient('Pepper', 1)
+      ]
     ),
   ];
 
