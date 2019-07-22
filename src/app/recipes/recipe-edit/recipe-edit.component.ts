f8721972d5895bd3e27fc4b69ff9ceafe986ac12
pako herrera <pakman_198@hotmail.com>
2019-07-22 11:38:32 -0700

    Added a reactive form to create and update recipes. Changed Added extra code to be able to access url segments and trigger editMode property on the recipe-edit component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
+
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +13,52 @@ import { ActivatedRoute } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   isEditMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.route.url.subscribe(segments => {
+      segments.forEach(segment => {
+        console.log({segment})
+        if(segment.path === 'edit') {
+          this.isEditMode = true;
+        }
+      })
+    });
+
     this.route.params.subscribe(params => {
+      console.log(this.route)
       this.id = +params.id;
-      this.isEditMode = params.edit != null
+      // this.isEditMode = params.edit != null
+      this.initForm();
+    });
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeDescription = '';
+    let recipeImagePath = '';
+    let recipeIngredients;
+
+    console.log('this.isEditMode', this.isEditMode)
+
+    if(this.isEditMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeDescription = recipe.description;
+      recipeImagePath = recipe.imagePath; 
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'description': new FormControl(recipeDescription),
+      'imagePath': new FormControl(recipeImagePath)
     })
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
