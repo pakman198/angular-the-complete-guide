67992dc32a1cbb8ce04ed9dd02466ec245f9a42c
pako herrera <pakman_198@hotmail.com>
2019-07-24 12:37:52 -0700

    Created an Authenticate route so user can login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,21 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolverService } from './recipes/recipe-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
- { path: 'recipes', component: RecipesComponent, children: [
-     {path: '', component: RecipeStartComponent },
-     {path: 'new', component: RecipeEditComponent },
-     // dynamic routes should come at the end, otherwise
-    //  a problem like trying to fetch data using "new" could occur
-     {path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
-     {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
- ]},
- { path: 'shopping-list', component: ShoppingListComponent },
- { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent },
+        {path: 'new', component: RecipeEditComponent },
+        // dynamic routes should come at the end, otherwise
+        //  a problem like trying to fetch data using "new" could occur
+        {path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolverService] },
+        {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipeResolverService] },
+    ]},
+    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent },
+ 
 ];
 
 @NgModule({
