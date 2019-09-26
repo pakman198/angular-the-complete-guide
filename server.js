0823b39887fe723a338cf33709281be42a449cbc
pako herrera <pakman_198@hotmail.com>
2019-09-25 22:36:43 -0700

    changes in package.json to deploy in heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/udemy-angular-the-complete-guide'));
+app.use(express.static(__dirname + '/dist/browser'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/udemy-angular-the-complete-guide/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/browser/index.html'));
 });
 
 // default Heroku PORT
